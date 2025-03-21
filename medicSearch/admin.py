2d4675327d8b6ef61f4175af829a6882e7875b5d
ALEXTUSANTOS1 <alextuliosilva@gmail.com>
2025-03-20 21:42:35 -0300

    atualização

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = ('user', 'role', 'birthday')
+    empty_value_display = '-empty-'
+    list_display_links = ('user','role')
+    list_filter = ('user__is_active', 'role')
+    fields = ('user', ('role',), 'image', 'birthday', 'specialities', 'addresses',)
+    exclude = ('favorites', 'created_at', 'updated_at',)
+    readonly_fields = ('user',)
+    search_fields = ('user__username',)
+
+
+
+
+
+
+
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
