23a2bfc8ca660817be8ee86695d171e90b80d8dd
ALEXTUSANTOS1 <alextuliosilva@gmail.com>
2025-03-23 21:25:14 -0300

    Update

--- medicSearchAdmin/urls.py
+++ medicSearchAdmin/urls.py
@@ -20,6 +20,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('medicSearch.urls.HomeUrls')),
+    path('profile/', include('medicSearch.urls.ProfileUrls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
